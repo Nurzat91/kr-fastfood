2809dad70a553e41aecba6ecdef43f4af0242406
Nurzat Taalaibekova <tnurzat91@gmail.com>
2023-11-04 09:19:22 +0300

    displaying menu on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import { useState } from 'react';
-
 import './App.css'
+import {useState} from 'react';
+import {Props} from './types';
 
 function App() {
+  const [menu, setmenu] = useState<Props[]>([
+    {id: 1, name: 'Hamburger', price: 100},
+    {id: 2, name: 'Chicken burger', price: 120},
+    {id: 1, name: 'Pita Burger', price: 150},
+    {id: 1, name: 'Coffee', price: 120},
+    {id: 1, name: 'Fresh', price: 150},
+    {id: 1, name: 'Tea', price: 50},
+  ]);
 
   return (
     <>
+      <div>Order Details:
 
+      </div>
+      <div>Add items:
+          {menu.map(item =>(
+              <div key={item.id} className="card">
+                <img/>
+                <div>
+                    <p>{item.name}</p>
+                    <p>Price: {item.price} KGS</p>
+                </div>
+              </div>
+          ))}
+      </div>
     </>
   )
 }
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+export interface Props{
+    id: number;
+    name: string;
+    price: number
+}
